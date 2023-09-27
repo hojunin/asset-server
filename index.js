999e4314acc2c7c968fe487b91d84c5e651c3201
hojunin <dlsghwns@naver.com>
2023-09-27 23:47:39 +0900

    feat: get요청 테스트

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("images"));
+app.use(express.static('images'));
+app.get('/test', (req, res) => {
+  res.send('GET 요청이 수신되었습니다.');
+});
 app.listen(4000);
